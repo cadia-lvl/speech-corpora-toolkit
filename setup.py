07bf61cfe045140ce4fe9d8c74813a2373938e72
staffan <staffanh@ru.is>
2022-02-17 08:51:12 +0000

    file headers and switch vlc to ffmpeg

--- setup.py
+++ setup.py
@@ -1,3 +1,13 @@
+#!/usr/bin/env python3
+
+"""
+    This is the setup file, allows this to be installed as a package.
+"""
+
+___author___ = "Staffan Hedström"
+___license___ = "Apache 2.0"
+___copyright___ = "2022 Staffan Hedström Reykjavík University"
+
 from setuptools import setup, find_packages
 
 setup(name="speech-corpus-tools", version="1.0", packages=find_packages())
